d0861d716a6341999f4366d0bda4454153c832f4
Anmol <anmolpokhrel101@gmail.com>
2023-10-14 15:00:49 +0545

    Initializing

---
+++ Login.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+    public class Login extends JFrame {
+    Login(){
+        setSize(800,1000);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Login();
+    }
+}
+
+
